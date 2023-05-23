a163e55b99f2330d6b52a18494636e5f43b96eb8
Rohit <rohitspujari@gmail.com>
2023-05-22 22:20:25 -0500

    added google login

--- app/authentication/Auth.tsx
+++ app/authentication/Auth.tsx
@@ -3,7 +3,7 @@ import { useAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import '../amplify_styles.css';
 
-//https://codesandbox.io/s/match-your-brand-vdwk0?file=/src/App.js
+// Reference project: https://codesandbox.io/s/match-your-brand-vdwk0?file=/src/App.js
 import AuthScreen from './AuthScreen';
 
 export default function Auth({ children }: { children: React.ReactNode }) {
