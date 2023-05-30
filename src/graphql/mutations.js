bb487ea53658f85fac623f360823a04bad38d1ab
Kevin Old <kevin@kevinold.com>
2023-05-30 14:00:20 -0500

    update schema

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,6 +1,84 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const createUser = /* GraphQL */ `
+  mutation CreateUser(
+    $input: CreateUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    createUser(input: $input, condition: $condition) {
+      id
+      username
+      email
+      projects {
+        items {
+          id
+          userId
+          projectId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
+      owner
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateUser = /* GraphQL */ `
+  mutation UpdateUser(
+    $input: UpdateUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    updateUser(input: $input, condition: $condition) {
+      id
+      username
+      email
+      projects {
+        items {
+          id
+          userId
+          projectId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
+      owner
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteUser = /* GraphQL */ `
+  mutation DeleteUser(
+    $input: DeleteUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    deleteUser(input: $input, condition: $condition) {
+      id
+      username
+      email
+      projects {
+        items {
+          id
+          userId
+          projectId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
+      owner
+      createdAt
+      updatedAt
+    }
+  }
+`;
 export const createProject = /* GraphQL */ `
   mutation CreateProject(
     $input: CreateProjectInput!
@@ -9,8 +87,34 @@ export const createProject = /* GraphQL */ `
     createProject(input: $input, condition: $condition) {
       id
       name
+      description
+      database
+      prompt {
+        items {
+          id
+          name
+          template
+          createdAt
+          updatedAt
+          projectPromptId
+          owner
+        }
+        nextToken
+      }
+      users {
+        items {
+          id
+          userId
+          projectId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
       createdAt
       updatedAt
+      owner
     }
   }
 `;
@@ -22,8 +126,34 @@ export const updateProject = /* GraphQL */ `
     updateProject(input: $input, condition: $condition) {
       id
       name
+      description
+      database
+      prompt {
+        items {
+          id
+          name
+          template
+          createdAt
+          updatedAt
+          projectPromptId
+          owner
+        }
+        nextToken
+      }
+      users {
+        items {
+          id
+          userId
+          projectId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
       createdAt
       updatedAt
+      owner
     }
   }
 `;
@@ -35,8 +165,250 @@ export const deleteProject = /* GraphQL */ `
     deleteProject(input: $input, condition: $condition) {
       id
       name
+      description
+      database
+      prompt {
+        items {
+          id
+          name
+          template
+          createdAt
+          updatedAt
+          projectPromptId
+          owner
+        }
+        nextToken
+      }
+      users {
+        items {
+          id
+          userId
+          projectId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
       createdAt
       updatedAt
+      owner
+    }
+  }
+`;
+export const createPrompt = /* GraphQL */ `
+  mutation CreatePrompt(
+    $input: CreatePromptInput!
+    $condition: ModelPromptConditionInput
+  ) {
+    createPrompt(input: $input, condition: $condition) {
+      id
+      name
+      template
+      project {
+        id
+        name
+        description
+        database
+        prompt {
+          nextToken
+        }
+        users {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      projectPromptId
+      owner
+    }
+  }
+`;
+export const updatePrompt = /* GraphQL */ `
+  mutation UpdatePrompt(
+    $input: UpdatePromptInput!
+    $condition: ModelPromptConditionInput
+  ) {
+    updatePrompt(input: $input, condition: $condition) {
+      id
+      name
+      template
+      project {
+        id
+        name
+        description
+        database
+        prompt {
+          nextToken
+        }
+        users {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      projectPromptId
+      owner
+    }
+  }
+`;
+export const deletePrompt = /* GraphQL */ `
+  mutation DeletePrompt(
+    $input: DeletePromptInput!
+    $condition: ModelPromptConditionInput
+  ) {
+    deletePrompt(input: $input, condition: $condition) {
+      id
+      name
+      template
+      project {
+        id
+        name
+        description
+        database
+        prompt {
+          nextToken
+        }
+        users {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      projectPromptId
+      owner
+    }
+  }
+`;
+export const createUserProjects = /* GraphQL */ `
+  mutation CreateUserProjects(
+    $input: CreateUserProjectsInput!
+    $condition: ModelUserProjectsConditionInput
+  ) {
+    createUserProjects(input: $input, condition: $condition) {
+      id
+      userId
+      projectId
+      user {
+        id
+        username
+        email
+        projects {
+          nextToken
+        }
+        owner
+        createdAt
+        updatedAt
+      }
+      project {
+        id
+        name
+        description
+        database
+        prompt {
+          nextToken
+        }
+        users {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const updateUserProjects = /* GraphQL */ `
+  mutation UpdateUserProjects(
+    $input: UpdateUserProjectsInput!
+    $condition: ModelUserProjectsConditionInput
+  ) {
+    updateUserProjects(input: $input, condition: $condition) {
+      id
+      userId
+      projectId
+      user {
+        id
+        username
+        email
+        projects {
+          nextToken
+        }
+        owner
+        createdAt
+        updatedAt
+      }
+      project {
+        id
+        name
+        description
+        database
+        prompt {
+          nextToken
+        }
+        users {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const deleteUserProjects = /* GraphQL */ `
+  mutation DeleteUserProjects(
+    $input: DeleteUserProjectsInput!
+    $condition: ModelUserProjectsConditionInput
+  ) {
+    deleteUserProjects(input: $input, condition: $condition) {
+      id
+      userId
+      projectId
+      user {
+        id
+        username
+        email
+        projects {
+          nextToken
+        }
+        owner
+        createdAt
+        updatedAt
+      }
+      project {
+        id
+        name
+        description
+        database
+        prompt {
+          nextToken
+        }
+        users {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
     }
   }
 `;
--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -1,33 +1,414 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const onCreateProject = /* GraphQL */ `
-  subscription OnCreateProject($filter: ModelSubscriptionProjectFilterInput) {
-    onCreateProject(filter: $filter) {
+export const onCreateUser = /* GraphQL */ `
+  subscription OnCreateUser(
+    $filter: ModelSubscriptionUserFilterInput
+    $owner: String
+  ) {
+    onCreateUser(filter: $filter, owner: $owner) {
       id
-      name
+      username
+      email
+      projects {
+        items {
+          id
+          userId
+          projectId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
+      owner
       createdAt
       updatedAt
+    }
+  }
+`;
+export const onUpdateUser = /* GraphQL */ `
+  subscription OnUpdateUser(
+    $filter: ModelSubscriptionUserFilterInput
+    $owner: String
+  ) {
+    onUpdateUser(filter: $filter, owner: $owner) {
+      id
+      username
+      email
+      projects {
+        items {
+          id
+          userId
+          projectId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
+      owner
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteUser = /* GraphQL */ `
+  subscription OnDeleteUser(
+    $filter: ModelSubscriptionUserFilterInput
+    $owner: String
+  ) {
+    onDeleteUser(filter: $filter, owner: $owner) {
+      id
+      username
+      email
+      projects {
+        items {
+          id
+          userId
+          projectId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
+      owner
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreateProject = /* GraphQL */ `
+  subscription OnCreateProject(
+    $filter: ModelSubscriptionProjectFilterInput
+    $owner: String
+  ) {
+    onCreateProject(filter: $filter, owner: $owner) {
+      id
+      name
+      description
+      database
+      prompt {
+        items {
+          id
+          name
+          template
+          createdAt
+          updatedAt
+          projectPromptId
+          owner
+        }
+        nextToken
+      }
+      users {
+        items {
+          id
+          userId
+          projectId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+      owner
     }
   }
 `;
 export const onUpdateProject = /* GraphQL */ `
-  subscription OnUpdateProject($filter: ModelSubscriptionProjectFilterInput) {
-    onUpdateProject(filter: $filter) {
+  subscription OnUpdateProject(
+    $filter: ModelSubscriptionProjectFilterInput
+    $owner: String
+  ) {
+    onUpdateProject(filter: $filter, owner: $owner) {
       id
       name
+      description
+      database
+      prompt {
+        items {
+          id
+          name
+          template
+          createdAt
+          updatedAt
+          projectPromptId
+          owner
+        }
+        nextToken
+      }
+      users {
+        items {
+          id
+          userId
+          projectId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
       createdAt
       updatedAt
+      owner
     }
   }
 `;
 export const onDeleteProject = /* GraphQL */ `
-  subscription OnDeleteProject($filter: ModelSubscriptionProjectFilterInput) {
-    onDeleteProject(filter: $filter) {
+  subscription OnDeleteProject(
+    $filter: ModelSubscriptionProjectFilterInput
+    $owner: String
+  ) {
+    onDeleteProject(filter: $filter, owner: $owner) {
       id
       name
+      description
+      database
+      prompt {
+        items {
+          id
+          name
+          template
+          createdAt
+          updatedAt
+          projectPromptId
+          owner
+        }
+        nextToken
+      }
+      users {
+        items {
+          id
+          userId
+          projectId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
       createdAt
       updatedAt
+      owner
+    }
+  }
+`;
+export const onCreatePrompt = /* GraphQL */ `
+  subscription OnCreatePrompt(
+    $filter: ModelSubscriptionPromptFilterInput
+    $owner: String
+  ) {
+    onCreatePrompt(filter: $filter, owner: $owner) {
+      id
+      name
+      template
+      project {
+        id
+        name
+        description
+        database
+        prompt {
+          nextToken
+        }
+        users {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      projectPromptId
+      owner
+    }
+  }
+`;
+export const onUpdatePrompt = /* GraphQL */ `
+  subscription OnUpdatePrompt(
+    $filter: ModelSubscriptionPromptFilterInput
+    $owner: String
+  ) {
+    onUpdatePrompt(filter: $filter, owner: $owner) {
+      id
+      name
+      template
+      project {
+        id
+        name
+        description
+        database
+        prompt {
+          nextToken
+        }
+        users {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      projectPromptId
+      owner
+    }
+  }
+`;
+export const onDeletePrompt = /* GraphQL */ `
+  subscription OnDeletePrompt(
+    $filter: ModelSubscriptionPromptFilterInput
+    $owner: String
+  ) {
+    onDeletePrompt(filter: $filter, owner: $owner) {
+      id
+      name
+      template
+      project {
+        id
+        name
+        description
+        database
+        prompt {
+          nextToken
+        }
+        users {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      projectPromptId
+      owner
+    }
+  }
+`;
+export const onCreateUserProjects = /* GraphQL */ `
+  subscription OnCreateUserProjects(
+    $filter: ModelSubscriptionUserProjectsFilterInput
+    $owner: String
+  ) {
+    onCreateUserProjects(filter: $filter, owner: $owner) {
+      id
+      userId
+      projectId
+      user {
+        id
+        username
+        email
+        projects {
+          nextToken
+        }
+        owner
+        createdAt
+        updatedAt
+      }
+      project {
+        id
+        name
+        description
+        database
+        prompt {
+          nextToken
+        }
+        users {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onUpdateUserProjects = /* GraphQL */ `
+  subscription OnUpdateUserProjects(
+    $filter: ModelSubscriptionUserProjectsFilterInput
+    $owner: String
+  ) {
+    onUpdateUserProjects(filter: $filter, owner: $owner) {
+      id
+      userId
+      projectId
+      user {
+        id
+        username
+        email
+        projects {
+          nextToken
+        }
+        owner
+        createdAt
+        updatedAt
+      }
+      project {
+        id
+        name
+        description
+        database
+        prompt {
+          nextToken
+        }
+        users {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onDeleteUserProjects = /* GraphQL */ `
+  subscription OnDeleteUserProjects(
+    $filter: ModelSubscriptionUserProjectsFilterInput
+    $owner: String
+  ) {
+    onDeleteUserProjects(filter: $filter, owner: $owner) {
+      id
+      userId
+      projectId
+      user {
+        id
+        username
+        email
+        projects {
+          nextToken
+        }
+        owner
+        createdAt
+        updatedAt
+      }
+      project {
+        id
+        name
+        description
+        database
+        prompt {
+          nextToken
+        }
+        users {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
     }
   }
 `;
